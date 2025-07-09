0016f03ad804516983aa579d056223c547493f64
Gunjan-pandey85340 <gunjanpandey8979@gmail.com>
2025-07-09 17:02:41 +0530

    modify_css

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
-
+const ejsMate = require("ejs-mate");
 const MONGO_URL = "mongodb://localhost:27017/wanderlust";
 
 main()
@@ -25,7 +25,8 @@ app.set("view engine","ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
-
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/",(req,res) => {
     res.send("Hi,I am root");
